5803698353e4c6aaf90c4eb48120ea8821bf99d0
SSK137 <saurabhkodam@gmail.com>
2022-06-13 23:16:47 +0530

    Create New Class in JS file

--- EmployeePayroll.js
+++ EmployeePayroll.js
@@ -1,1 +1,17 @@
-console.log("Welcome To Employee Payroll Service");
+console.log("Welcome to Employee Payroll Using Java Script");
+class EmployeePayroll{
+    constructor(id, name, salary){
+        this.id = id;
+        this.name = name;
+        this.salary = salary;
+    }
+
+    get name(){ return this._name; }
+    set name(name){ this._name = name; }
+
+    toString(){
+        return "id: "+this.id+" name: "+this.name+" salary:"+this.salary;
+    }
+}
+let employeePayroll = new EmployeePayroll(1, "SSK", 35000);
+console.log(employeePayroll.toString());
